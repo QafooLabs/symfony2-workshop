27e9d5990e0c941c21ce48a8f87640d50a77d250
Benjamin Eberlei <kontakt@beberlei.de>
2013-07-03 14:15:31 +0200

    Add KernelTestCase and commented security config

---
+++ src/Qafoo/WorkshopBundle/Tests/KernelTestCase.php
@@ -0,0 +1,27 @@
+<?php
+namespace Qafoo\WorkshopBundle\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+abstract class KernelTestCase extends WebTestCase
+{
+    protected $container;
+
+    public function setUp()
+    {
+        $config = array('environment'=>'test', 'debug'=>true);
+        self::$kernel = self::createKernel($config);
+        self::$kernel−>boot();
+
+        $this->container = self::$kernel->getContainer();
+    }
+
+    public function tearDown()
+    {
+        if (self::$kernel === null) {
+            return;
+        }
+
+        self::$kernel->shutdown();
+    }
+}
